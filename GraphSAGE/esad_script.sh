5fb6956c601891d6b4d9995f3ddf0cb694ecf896
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-08 01:56:29 +0300

    logical ordering

--- GraphSAGE/esad_script.sh
+++ GraphSAGE/esad_script.sh
@@ -3,16 +3,31 @@
 Models="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq n2v"
 
 
-for model in $Models
+for epochs in 10 20 30
 do
-    for epochs in 10 20 30
+    for validate_iter in 10 20
     do
-        for validate_iter in 10 20
-        do
-            for identity_dim in 0 32 64 128
+        for identity_dim in 0 32 64 128
+        do            
+            for model in $Models
             do
                 mkdir -p "king_esad/$model/$epochs/$validate_iter/$identity_dim"
-                python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --train_prefix ${train_prefix} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --max_total_steps 10000 --base_log_dir "./king_esad/$model/$epochs/$validate_iter/$identity_dim" --gpu 0
+                python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --train_prefix ${train_prefix} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --max_total_steps 600 --base_log_dir "./king_esad/$model/$epochs/$validate_iter/$identity_dim" --gpu 0
+            done
+        done
+    done
+done
+
+for epochs in 10 20 30
+do
+    for validate_iter in 10 20
+    do
+        for identity_dim in 0 32 64 128
+        do            
+            for model in $Models
+            do
+                mkdir -p "king_esad_supervised/$model/$epochs/$validate_iter/$identity_dim"
+                python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --train_prefix ${train_prefix} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --sigmoid --max_total_steps 600 --base_log_dir "./king_esad/$model/$epochs/$validate_iter/$identity_dim" --gpu 0
             done
         done
     done
