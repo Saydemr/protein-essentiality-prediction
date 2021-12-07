fb8906160d44e9bffe9b255d866bb5f4116f258c
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-08 02:01:47 +0300

    logical ordering

--- GraphSAGE/esad_script.sh
+++ GraphSAGE/esad_script.sh
@@ -3,16 +3,36 @@
 Models="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq n2v"
 
 
-for model in $Models
+for epochs in 10 20 30
 do
-    for epochs in 10 20 30
+    for validate_iter in 10 20
     do
-        for validate_iter in 10 20
+        for identity_dim in 0 32 64 128
         do
-            for identity_dim in 0 32 64 128
+            for model in $Models
             do
-                mkdir -p "king_esad/$model/$epochs/$validate_iter/$identity_dim"
-                python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --train_prefix ${train_prefix} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --max_total_steps 10000 --base_log_dir "./king_esad/$model/$epochs/$validate_iter/$identity_dim" --gpu 0
+                my_dir="king_esad/${model}/${epochs}/${validate_iter}/${identity_dim}"
+                mkdir -p ${my_dir}
+                python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --max_total_steps 600 --gpu 0
+            done
+        done
+    done
+done
+
+
+SupModels="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq"
+
+for epochs in 10 20 30
+do
+    for validate_iter in 10 20
+    do
+        for identity_dim in 0 32 64 128
+        do
+            for model in $SupModels
+            do
+                my_dir="king_esad_supervised/${model}/${epochs}/${validate_iter}/${identity_dim}"
+                mkdir -p ${my_dir}
+                python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --sigmoid --max_total_steps 600 --gpu 0
             done
         done
     done
