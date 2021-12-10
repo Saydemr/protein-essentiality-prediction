ecc22f405dfc6f5e97b5778a6a11b12de957c19a
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-10 23:47:15 +0300

    Script latest

--- GraphSAGE/esad_script.sh
+++ GraphSAGE/esad_script.sh
@@ -7,11 +7,11 @@ for epochs in 10 20 30
 do
     for validate_iter in 10 20
     do
-        for identity_dim in 0 32 64 128
+        for identity_dim in 64 128 32 0
         do
             for model in $Models
             do
-                my_dir="king_esad/${model}/${epochs}/${validate_iter}/${identity_dim}"
+                my_dir="king_esad/e${epochs}_iter${validate_iter}_id${identity_dim}"
                 mkdir -p ${my_dir}
                 python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --max_total_steps 600 --gpu 0
             done
@@ -26,13 +26,13 @@ for epochs in 10 20 30
 do
     for validate_iter in 10 20
     do
-        for identity_dim in 0 32 64 128
+        for identity_dim in 64 128 32 0
         do
             for model in $SupModels
             do
-                my_dir="king_esad_supervised/${model}/${epochs}/${validate_iter}/${identity_dim}"
+                my_dir="king_esad_supervised/e${epochs}_iter${validate_iter}_id${identity_dim}"
                 mkdir -p ${my_dir}
-                python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --sigmoid --max_total_steps 600 --gpu 0
+                python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --max_total_steps 600 --gpu 0
             done
         done
     done
