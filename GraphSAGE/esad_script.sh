f78c8aee384fae3c4f810bb304e07ba0b1aa349d
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-11 10:49:07 +0300

    script, again...

--- GraphSAGE/esad_script.sh
+++ GraphSAGE/esad_script.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-Models="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq n2v"
-
+SupModels="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq"
 
 for epochs in 10 20 30
 do
-    for validate_iter in 10 20
+    for batch_size in 16 32 64 128
     do
         for identity_dim in 64 128 32 0
         do
-            for model in $Models
+            for lr in 0.01 0.005 0.001 0.05
             do
-                my_dir="king_esad/e${epochs}_iter${validate_iter}_id${identity_dim}"
-                mkdir -p ${my_dir}
-                python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --max_total_steps 600 --gpu 0
+                for model in $SupModels
+                do
+                    my_dir="king_esad-supervised/e${epochs}_b${batch_size}_id${identity_dim}_lr{lr}"
+                    mkdir -p ${my_dir}
+                    python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --learning_rate ${lr} --base_log_dir ${my_dir} --max_total_steps 10100 --gpu 0
+                done
             done
         done
     done
 done
 
-
-SupModels="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq"
+Models="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq n2v"
 
 for epochs in 10 20 30
 do
-    for validate_iter in 10 20
+    for batch_size in 16 32 64 128
     do
         for identity_dim in 64 128 32 0
         do
-            for model in $SupModels
+            for lr in 0.01 0.005 0.001 0.05
             do
-                my_dir="king_esad_supervised/e${epochs}_iter${validate_iter}_id${identity_dim}"
-                mkdir -p ${my_dir}
-                python -m graphsage.supervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --validate_iter ${validate_iter} --base_log_dir ${my_dir} --max_total_steps 600 --gpu 0
+                for model in $Models
+                do
+                    my_dir="king_esad/e${epochs}_b${batch_size}_id${identity_dim}_lr{lr}"
+                    mkdir -p ${my_dir}
+                    python -m graphsage.unsupervised_train --train_prefix ./example_data/eppugnn --model ${model} --epochs ${epochs} --identity_dim ${identity_dim} --learning_rate ${lr} --base_log_dir ${my_dir} --max_total_steps 10100 --gpu 0
+                done
             done
         done
     done
