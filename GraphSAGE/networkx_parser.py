a6eae79718f71dd7e2d835b69019ec169802ad81
Burcu Arslan <burcu.arslan@ozu.edu.tr>
2021-12-29 18:14:44 +0300

    Grand inputs

--- GraphSAGE/networkx_parser.py
+++ GraphSAGE/networkx_parser.py
@@ -5,8 +5,10 @@ from random import choices
 from collections import Counter
 import json
 import numpy as np
+import scipy as sp
+from scipy import sparse
 
-exit()
+
 print("Loading graph...")
 ppi_graph = nx.Graph() 
 
@@ -14,6 +16,9 @@ id_map = {}
 id_map_int = {}
 id_map_inv = {}
 id_map_inv_int = {}
+id_name_dict ={}
+
+np_adj_matrix = np.zeros((7349,7349), dtype=np.int8)
 
 with open("BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt") as f:
     f.readline()
@@ -23,7 +28,10 @@ with open("BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt") as
         line = line.split("\t")
         if line[3] == line[4]:
             continue
-
+        
+        id_name_dict[line[3]] = line[7]
+        id_name_dict[line[4]] = line[8]
+        
         if int(line[3]) not in id_map_int and int(line[4]) not in id_map_int:
             ppi_graph.add_edge(i,i+1)
 
@@ -39,6 +47,8 @@ with open("BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt") as
             id_map_inv_int[i+1] = int(line[4])
             ppi_graph.nodes[i+1]['id'] = i+1
             
+            np_adj_matrix[i][i+1] = 1
+            np_adj_matrix[i+1][i] = 1
             #print(i, i+1)
             i+=2
 
@@ -53,9 +63,12 @@ with open("BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt") as
 
             ppi_graph.nodes[i]['id'] = i
             #print(id_map_int[int(line[3])], i)
+            
+            np_adj_matrix[id_map_int[int(line[3])]][i] = 1
+            np_adj_matrix[i][id_map_int[int(line[3])]] = 1
+            
             i+=1
 
-            
         elif int(line[3]) not in id_map_int and int(line[4]) in id_map_int:
             
             ppi_graph.add_edge(i,id_map_int[int(line[4])])
@@ -65,18 +78,24 @@ with open("BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt") as
             id_map_inv[i] = line[3]
             id_map_inv_int[i] = int(line[3])
             ppi_graph.nodes[i]['id'] = i
-            
+
+            np_adj_matrix[i][id_map_int[int(line[4])]] = 1
+            np_adj_matrix[id_map_int[int(line[4])]][i] = 1            
             #print(i, id_map_int[int(line[4])])
+
             i+=1
 
         else:
             ppi_graph.add_edge(id_map_int[int(line[3])],id_map_int[int(line[4])])
+            np_adj_matrix[id_map_int[int(line[3])]][id_map_int[int(line[4])]] = 1
+            np_adj_matrix[id_map_int[int(line[4])]][id_map_int[int(line[3])]] = 1
 
 
+
+sp.sparse.save_npz('../grand_blend/adj_matrix_grand.npz', sp.sparse.csr_matrix(np_adj_matrix))
 # 0 : training = train_removed false : test_removed true
 # 1 : test     = train_removed true  : test_removed false
 # 2 : validation = true true
-
 
 print("Graph info...")
 print("Number of nodes: ", ppi_graph.number_of_nodes())
@@ -84,16 +103,32 @@ print("Number of connected components", nx.number_connected_components(ppi_graph
 print("Number of edges: ", ppi_graph.number_of_edges())
 print()
 
+
+# # These lines are not needed for the current dataset
+# for component in list(nx.connected_components(ppi_graph)):
+#     if len(component) < 3:
+#         for node in component:
+#             ppi_graph.remove_node(node)
+
+# print("Checking the graph if smth is modified.")
+# print("Number of nodes: ", ppi_graph.number_of_nodes())
+# print("Number of connected components", nx.number_connected_components(ppi_graph))
+# print("Number of edges: ", ppi_graph.number_of_edges())
+# print()
+
 with open("sc_ppi_graph.txt", "w+") as f:
     for e in ppi_graph.edges():
         a,b = e
         f.write(str(id_map_inv_int[a]) + " " + str(id_map_inv_int[b]) + "\n")
 
+
 population = [0, 1, 2]
 weights    = [0.8, 0.1, 0.1]
 distribution_samples = choices(population, weights, k=ppi_graph.number_of_nodes())
+
 print("Number of instances in training (0), test (1) and validation (2)\n",Counter(distribution_samples),sep='\n')
 print("Number of instances in training (0), test (1) and validation (2)\n",Counter(distribution_samples),sep='\n', file=open("distribution_samples.txt", "w+"))
+
 for i in range(ppi_graph.number_of_nodes()):
     if distribution_samples[i] == 0:
         ppi_graph.nodes[i]['test'] = False 
@@ -108,23 +143,7 @@ for i in range(ppi_graph.number_of_nodes()):
     #print(i)
     #print(ppi_graph.nodes[id_map_inv[i]]['test'], ppi_graph.nodes[id_map_inv[i]]['val'], sep="\t", end="\n")
 
-print("Checking the graph if smth is modified.")
-print("Number of nodes: ", ppi_graph.number_of_nodes())
-print("Number of connected components", nx.number_connected_components(ppi_graph))
-print("Number of edges: ", ppi_graph.number_of_edges())
-print()
-
 print("Creating class-map")
-id_name_dict ={}
-with open('BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.204.tab3.txt') as f:
-    f.readline()
-    for line in f:
-        line = line.strip().split('\t')
-        if line[3] == line[4]:
-            continue
-        id_name_dict[line[3]] = line[7]
-        id_name_dict[line[4]] = line[8]
-
 essential_dict = set()
 with open('deg_sc.dat') as f:
     for line in f:
@@ -134,14 +153,17 @@ with open('deg_sc.dat') as f:
 
 class_map = {}
 #print(id_map)
+y_mat = np.zeros((ppi_graph.number_of_nodes(),1), dtype=np.int8)
 for i in id_map:
     my_key = id_map[i]
     my_str = id_name_dict[i]
     if my_str in essential_dict:
         class_map[my_key] = 1
+        y_mat[my_key] = 1
     else:
         class_map[my_key] = 0
 
+np.save('../grand_blend/y_mat_grand.npy', y_mat)
 #print(class_map)
 
 print('Creating id-map')
@@ -150,6 +172,7 @@ for i in range(ppi_graph.number_of_nodes()):
     id_mappppp[str(i)] = i
 
 print("Writing graph to JSON file...")
+
 json.dump(class_map, fp=open("sc_eppugnn-class_map.json", "w+"))
 json.dump(json_graph.node_link_data(ppi_graph), fp=open("sc_eppugnn-G.json", "w+"))
 json.dump({str(v): int(k) for k, v in id_map.items()}, fp=open("sc_eppugnn-id_map_inv.json", "w+"))
