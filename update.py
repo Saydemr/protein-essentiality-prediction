188b09fb948d71e6706ca6730dd7ac541d82c69a
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-04-11 01:02:37 +0300

    Skip downloading existing files

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 import subprocess
 import sys
 
@@ -29,6 +30,7 @@ def most_recent_available(version_number):
 
 # Get the current version
 files = fnmatch.filter(os.listdir('./data'), 'BIOGRID-ORGANISM-4.4.*.tab3.zip')
+print(len(files), files)
 if len(files) == 0:
     version_number = 206
     FLAG = False
@@ -42,9 +44,10 @@ most_recent = most_recent_available(version_number)
 if most_recent > version_number:
     
     # Remove the old files if exist
-    print("Downloading the newest BİOGRID version")
+    print("Downloading the newest BioGRID version")
     if FLAG:
         files = fnmatch.filter(os.listdir('./data'), 'BIOGRID-ORGANISM-*.tab3*')
+        print(files)
         for file in files:
             os.remove('./data/' + file)
 
@@ -68,80 +71,88 @@ else:
 
 # Download gene expression data
 print("Downloading gene expression data")
-expression_human = requests.get('https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE86354&format=file&file=GSE86354_GTEx_FPKM.txt.gz', stream=True)
-with open('./data/GSE86354_GTEx_FPKM.txt.gz', 'wb') as f:
-    f.write(expression_human.content)
+if not isfile('./data/GSE86354_GTEx_FPKM.txt'):
+    expression_human = requests.get('https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE86354&format=file&file=GSE86354_GTEx_FPKM.txt.gz', stream=True)
+    with open('./data/GSE86354_GTEx_FPKM.txt.gz', 'wb') as f:
+        f.write(expression_human.content)
 
-with gzip.open('./data/GSE86354_GTEx_FPKM.txt.gz', 'rb') as f_in:
-    with open('./data/GSE86354_GTEx_FPKM.txt', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
+    with gzip.open('./data/GSE86354_GTEx_FPKM.txt.gz', 'rb') as f_in:
+        with open('./data/GSE86354_GTEx_FPKM.txt', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
-supplementary_hs = requests.get('https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?mode=raw&is_datatable=true&acc=GPL570&id=55999&db=GeoDb_blob143', stream=True)
-with open('./data/GPL570-55999.txt', 'wb') as f:
-    f.write(supplementary_hs.content)
+if not isfile('./data/GPL570-55999.txt'):
+    supplementary_hs = requests.get('https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?mode=raw&is_datatable=true&acc=GPL570&id=55999&db=GeoDb_blob143', stream=True)
+    with open('./data/GPL570-55999.txt', 'wb') as f:
+        f.write(supplementary_hs.content)
 
 
+if not isfile('./data/GSE3431_series_matrix.txt'):
+    expression_sc = requests.get('https://ftp.ncbi.nlm.nih.gov/geo/series/GSE3nnn/GSE3431/matrix/GSE3431_series_matrix.txt.gz', stream=True)
+    with open('./data/GSE3431_series_matrix.txt.gz', 'wb') as f:
+        f.write(expression_sc.content)
 
-expression_sc = requests.get('https://ftp.ncbi.nlm.nih.gov/geo/series/GSE3nnn/GSE3431/matrix/GSE3431_series_matrix.txt.gz', stream=True)
-with open('./data/GSE3431_series_matrix.txt.gz', 'wb') as f:
-    f.write(expression_sc.content)
+    with gzip.open('./data/GSE3431_series_matrix.txt.gz', 'rb') as f_in:
+        with open('./data/GSE3431_series_matrix.txt', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
-with gzip.open('./data/GSE3431_series_matrix.txt.gz', 'rb') as f_in:
-    with open('./data/GSE3431_series_matrix.txt', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-
-supplementary_sc = requests.get('https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?mode=raw&is_datatable=true&acc=GPL90&id=17389&db=GeoDb_blob144', stream=True)
-with open('./data/GPL90-17389.txt', 'wb') as f:
-    f.write(supplementary_sc.content)
+if not isfile('./data/GPL90-17389.txt'):
+    supplementary_sc = requests.get('https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?mode=raw&is_datatable=true&acc=GPL90&id=17389&db=GeoDb_blob144', stream=True)
+    with open('./data/GPL90-17389.txt', 'wb') as f:
+        f.write(supplementary_sc.content)
 
 
 # Download the subcellular localization data
 print("Downloading subcellular localization data")
-subcellular_hs = requests.get('https://download.jensenlab.org/human_compartment_knowledge_full.tsv', stream=True)
-with open('./data/human_compartment_knowledge_full.tsv', 'wb') as f:
-    f.write(subcellular_hs.content)
+if not isfile('./data/human_compartment_knowledge_full.tsv'):
+    subcellular_hs = requests.get('https://download.jensenlab.org/human_compartment_knowledge_full.tsv', stream=True)
+    with open('./data/human_compartment_knowledge_full.tsv', 'wb') as f:
+        f.write(subcellular_hs.content)
 
-subcellular_sc = requests.get('https://download.jensenlab.org/yeast_compartment_knowledge_full.tsv', stream=True)
-with open('./data/yeast_compartment_knowledge_full.tsv', 'wb') as f:
-    f.write(subcellular_sc.content)
+
+if not isfile('./data/yeast_compartment_knowledge_full.tsv'):
+    subcellular_sc = requests.get('https://download.jensenlab.org/yeast_compartment_knowledge_full.tsv', stream=True)
+    with open('./data/yeast_compartment_knowledge_full.tsv', 'wb') as f:
+        f.write(subcellular_sc.content)
 
 
 # Download essential genes data
 print("Downloading essential genes data")
-essential_genes = requests.get('http://tubic.tju.edu.cn/deg/download/deg-e-15.2.zip', stream=True)
-with open('./data/deg-e-15.2.zip', 'wb') as f:
-    f.write(essential_genes.content)
+
+if not isfile('./data/degannotation-e.dat') or not isfile('./data/deg_sc.dat'):
+    essential_genes = requests.get('http://tubic.tju.edu.cn/deg/download/deg-e-15.2.zip', stream=True)
+    with open('./data/deg-e-15.2.zip', 'wb') as f:
+        f.write(essential_genes.content)
 
 
-with ZipFile('./data/deg-e-15.2.zip', 'r') as z:
-    z.extract('degannotation-e.dat', './data/')
+    with ZipFile('./data/deg-e-15.2.zip', 'r') as z:
+        z.extract('degannotation-e.dat', './data/')
+
+    with open ('./data/degannotation-e.dat', 'r') as f:
+        with open ('./data/deg_sc.dat', 'w+') as g:
+            f.readline()
+            for line in f:
+                line = line.strip()
+                line = line.split('\t')
+                if line[7] == 'Saccharomyces cerevisiae':
+                    g.write(line[2] + '\n')
 
 
-with open ('./data/degannotation-e.dat', 'r') as f:
-    with open ('./data/deg_sc.dat', 'w+') as g:
+if not isfile('./data/deg_hs.dat'):
+    gene_num_listed = {}
+    with open ('./data/degannotation-e.dat', 'r') as f:
         f.readline()
         for line in f:
             line = line.strip()
             line = line.split('\t')
-            if line[7] == 'Saccharomyces cerevisiae':
-                g.write(line[2] + '\n')
+            if line[7] == 'Homo sapiens':
+                if line[2] in gene_num_listed:
+                    gene_num_listed[line[2]] += 1
+                else:
+                    gene_num_listed[line[2]] = 1
 
 
-gene_num_listed = {}
-with open ('./data/degannotation-e.dat', 'r') as f:
-    f.readline()
-    for line in f:
-        line = line.strip()
-        line = line.split('\t')
-        if line[7] == 'Homo sapiens':
-            if line[2] in gene_num_listed:
-                gene_num_listed[line[2]] += 1
-            else:
-                gene_num_listed[line[2]] = 1
-
-
-print("Preparing Homo Sapiens annotations...")
-with open ('./data/deg_hs.dat', 'w+') as g:
-    for key in gene_num_listed:
-        if gene_num_listed[key] > 4:
-            g.write(key + '\n')
+    print("Preparing Homo Sapiens annotations...")
+    with open ('./data/deg_hs.dat', 'w+') as g:
+        for key in gene_num_listed:
+            if gene_num_listed[key] > 4:
+                g.write(key + '\n')
