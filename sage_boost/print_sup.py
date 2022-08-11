432430c2166a9cabe204dd83449ec6efec2d94e4
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-08-11 14:36:56 +0300

    Update supervised printing

--- sage_boost/print_sup.py
+++ sage_boost/print_sup.py
@@ -1,20 +1,21 @@
 import os
 
-models = ['graphsage_maxpool', 'graphsage_meanpool']
-identity_dim = 32
-epoch = 50
-batch_size = 32
-v = 100
-vb = 512
-organism = "sc"
+models = ["gcn", "graphsage_maxpool", "graphsage_mean", "graphsage_meanpool", "graphsage_seq"]
+organism = "hs"
 with open('./log/logs_sup_new.csv', 'w+') as log_csv:
-    log_csv.write('Option,Model,LR,Accuracy,F1,ROC_AUC,Precision,Recall,TestorVal\n')
+    log_csv.write('Option,Model,Accuracy,F1,ROC_AUC,Precision,Recall,TestorVal\n')
     for model in models:
-        for learning_rate in ["0.0100", "0.0010", "0.0001"]:
-            for option in range(5):
-                base_dir = './supervised/org{}_e{}_b{}_v{}_vb{}_id{}_opt{}/sup-example_data/{}_small_{}/'.format(organism,epoch,batch_size,v,vb,identity_dim,option,model,learning_rate)
-                test_txt = base_dir + 'test_stats.txt'
-                if os.path.isfile(test_txt):
-                    with open(test_txt, 'r') as test_txt:
-                        test_line = test_txt.readline().strip().replace('=', ' ').split()
-                        log_csv.write('{},{},{},{},{},{},{},{},Test\n'.format(option,model,learning_rate,test_line[3],test_line[5],test_line[7],test_line[9],test_line[11]))
+        for option in range(5):
+            base_dir = './supervised_old/org{}_id_opt{}/sup-example_data/{}_small_{}/'.format(organism,option,model)
+            test_txt = base_dir + 'test_stats.txt'
+            if os.path.isfile(test_txt):
+                with open(test_txt, 'r') as test_txt:
+                    test_line = test_txt.readline().strip().replace('=', ' ').split()
+                    log_csv.write('{},{},{},{},{},{},{},{},Test\n'.format(option,model,test_line[3],test_line[5],test_line[7],test_line[9],test_line[11]))
+
+            val_txt = base_dir + 'val_stats.txt'
+            if os.path.isfile(val_txt):
+                with open(val_txt, 'r') as val_txt:
+                    val_line = val_txt.readline().strip().replace('=', ' ').split()
+                    log_csv.write('{},{},{},{},{},{},{},{},Val\n'.format(option,model,test_line[3],test_line[5],test_line[7],test_line[9],test_line[11]))
+    
