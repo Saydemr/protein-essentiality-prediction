2f3e6678c7acfcac4f48020da9c5b4aa8a92dd8e
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-09-24 20:52:27 +0300

    Add mus musculus Add min-size PCA Refactor graph parsing Comment out SAGE related code

--- data/params.py
+++ data/params.py
@@ -1,3 +1,4 @@
 params_dict = { 'hs': {'full_name': 'Homo_sapiens',             'deg' : 'deg_hs.dat', 'ge': 'GSE86354_GTEx_FPKM.txt',    'ge_parsed': 'GSE86354_GTEx_FPKM_gene.txt',    'pca' : 100,  'go': 'human_compartment_knowledge_full.tsv', 'map': 'GPL570-55999.txt'},
-                'sc': {'full_name': 'Saccharomyces_cerevisiae', 'deg' : 'deg_sc.dat', 'ge': 'GSE3431_series_matrix.txt', 'ge_parsed': 'GSE3431_series_matrix_gene.txt', 'pca' : 100,  'go': 'yeast_compartment_knowledge_full.tsv', 'map': 'GPL90-17389.txt'}
-              }
+                'sc': {'full_name': 'Saccharomyces_cerevisiae', 'deg' : 'deg_sc.dat', 'ge': 'GSE3431_series_matrix.txt', 'ge_parsed': 'GSE3431_series_matrix_gene.txt', 'pca' : 100,  'go': 'yeast_compartment_knowledge_full.tsv', 'map': 'GPL90-17389.txt'},
+                'mm': {'full_name': 'Mus_musculus',             'deg' : 'deg_mm.dat', 'ge': 'GSE9338_series_matrix.txt', 'ge_parsed': 'GSE9338_series_matrix_gene.txt', 'pca' : 100,  'go': 'mouse_compartment_knowledge_full.tsv', 'map': 'GPL1261-56135.txt'}
+}
